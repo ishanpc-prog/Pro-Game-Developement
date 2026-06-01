4de6036b4f5e47b7c2fd4ed450cf2641a7702a1b
ishanpc-prog <ishanpc02@gmail.com>
2026-06-01 16:37:11 +0200

    Add files via upload

--- Lesson 5 - Keyboard events.py
+++ Lesson 5 - Keyboard events.py
@@ -3,8 +3,8 @@ from pygame.locals import *
 import time
 
 pygame.init()
-WIDTH = 500
-HEIGHT = 500
+WIDTH = 700
+HEIGHT = 700
 
 screen = pygame.display.set_mode([WIDTH,HEIGHT])
 player_x = 200
@@ -12,11 +12,45 @@ player_y = 200
 
 keys = [False,False,False,False]
 rocket1 = pygame.image.load("images/rocket3.png")
+r1 = pygame.transform.scale(rocket1,(200,150))
 bg = pygame.image.load("images/space.png")
 
-while player_y < 500 or player_x < 500:
+while player_y < 490:
     screen.blit(bg,(0,0))
-    screen.blit(rocket1,(player_x,player_y))
+    screen.blit(r1,(player_x,player_y))
     pygame.display.flip()
     for event in pygame.event.get():
-        if event.type()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == K_UP:
+                keys[0] == True
+            elif event.key == K_DOWN:
+                keys[1] == True
+            elif event.key == K_LEFT:
+                keys[2] == True
+            elif event.key == K_RIGHT:
+                keys[3] == True
+        elif event.type == pygame.KEYUP:
+            if event.key == K_UP:
+                keys[0] == False
+            elif event.key == K_DOWN:
+                keys[1] == False
+            elif event.key == K_LEFT:
+                keys[2] == False
+            elif event.key == K_RIGHT:
+                keys[3] == False
+    if keys[0]:
+        if player_y > 0:
+            player_y = player_y - 5
+    elif keys[1]:
+        if player_y < 490:
+            player_y = player_y + 5
+    elif keys[2]:
+        if player_x > 0:
+            player_x = player_x - 5
+    elif keys[3]:
+        if player_x < 490:
+            player_x = player_x + 5
+    time.sleep(0.05)
+print("Game over!")
