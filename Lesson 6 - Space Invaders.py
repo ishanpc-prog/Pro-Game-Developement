8d93f5ce1df7fcf940cdb026f49ac21a0d592fa6
ishanpc-prog <ishanpc02@gmail.com>
2026-06-08 16:56:15 +0200

    Add files via upload

--- Lesson 6 - Space Invaders.py
+++ Lesson 6 - Space Invaders.py
@@ -1,1 +1,26 @@
 import pygame
+
+pygame.init()
+WIDTH = 700
+HEIGHT = 700
+pygame.display.set_caption("SPACE INVADERS")
+
+screen = pygame.display.set_mode([WIDTH,HEIGHT])
+red = (255,0,0)
+yellow = (0,255,255)
+black = (0,0,0)
+pygame.font.init()
+border = pygame.Rect(345,0,10,700)
+health_font = pygame.font.SysFont("Times New Roman", 40)
+winner_font = pygame.font.SysFont("Times New Roman", 100)
+fps = 60
+laser_velo = 8 # velo for velocity
+max_lasershots = 3
+battle_ship_velo = 5
+battle_ship_width = 55
+battle_ship_height = 40
+red_ship_image = pygame.image.load("images/red_battle_ship.png")
+red_ship_scale = pygame.transform.scale(red_ship_image,(battle_ship_width,battle_ship_height))
+red_ship = pygame.transform.rotate(red_ship_scale,90) # yellow will 270 deggrees
+yellow_ship_image = pygame.image.load("images/yellow_battle_ship.png")
+yellow_ship_scale = pygame.transform.scale()
