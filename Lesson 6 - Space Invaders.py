6e1b4d5ae4b54d1768c45c414501bd57a07ef8f4
ishanpc-prog <ishanpc02@gmail.com>
2026-07-20 17:00:54 +0200

    Add files via upload

--- Lesson 6 - Space Invaders.py
+++ Lesson 6 - Space Invaders.py
@@ -25,16 +25,12 @@ red_ship = pygame.transform.rotate(red_ship_scale,90) # yellow will 270 degrees 
 yellow_ship_image = pygame.image.load("images/yellow_battle_ship.png")
 yellow_ship_scale = pygame.transform.scale(yellow_ship_image,(battle_ship_width,battle_ship_height))
 yellow_ship = pygame.transform.rotate(yellow_ship_scale,270) #red ship left and yellow ship right
-red_ship_health = 20
-yellow_ship_health = 20
 winner = ""
 game_over = False
 game_over_font = pygame.font.SysFont("Times New Roman", 64)
 simple_font = pygame.font.SysFont("Times New Roman", 40)
 bg = pygame.image.load("images/space_bg_2.png")
 backround = pygame.transform.scale(bg,(WIDTH,HEIGHT))
-yellow_object = pygame.Rect(20,375,battle_ship_width,battle_ship_height) #Purpose: Storage of Position and Size
-red_object = pygame.Rect(680,375,battle_ship_width,battle_ship_height)   #Purpose: Storage of Position and Size
 yellow_hit = pygame.USEREVENT + 1
 red_hit = pygame.USEREVENT + 2
 
@@ -54,23 +50,23 @@ def draw_window(red,yellow,red_bullets,yellow_bullets,red_ship_health,yellow_shi
     pygame.display.update()
 
 def red_movement(key_pressed,red):
-    if key_pressed[pygame.K_a] and red.x - laser_velo > 0:
+    if key_pressed[pygame.K_LEFT] and red.x - laser_velo > 0:
         red.x = red.x - laser_velo
-    elif key_pressed[pygame.K_d] and red.width + red.x + laser_velo < border.x:
+    elif key_pressed[pygame.K_RIGHT] and red.width + red.x + laser_velo < border.x:
         red.x = red.x + laser_velo
-    elif key_pressed[pygame.K_w] and red.y - laser_velo > 0:
+    elif key_pressed[pygame.K_UP] and red.y - laser_velo > 0:
         red.y = red.y - laser_velo
-    elif key_pressed[pygame.K_s] and red.height + red.y + laser_velo < 685:
+    elif key_pressed[pygame.K_DOWN] and red.height + red.y + laser_velo < 685:
         red.y = red.y - laser_velo
 
 def yellow_movement(key_pressed,yellow):
-    if key_pressed[pygame.K_LEFT] and yellow.x - laser_velo > border.x + border.width:
+    if key_pressed[pygame.K_a] and yellow.x - laser_velo > border.x + border.width:
         yellow.x = yellow.x - laser_velo
-    elif key_pressed[pygame.K_RIGHT] and yellow.width + yellow.x + laser_velo < 685:
+    elif key_pressed[pygame.K_d] and yellow.width + yellow.x + laser_velo < 685:
         yellow.x = yellow.x + laser_velo
-    elif key_pressed[pygame.K_UP] and yellow.y - laser_velo > 0:
+    elif key_pressed[pygame.K_w] and yellow.y - laser_velo > 0:
         yellow.y = yellow.y - laser_velo
-    elif key_pressed[pygame.K_DOWN] and yellow.height + yellow.y + laser_velo < 685:
+    elif key_pressed[pygame.K_s] and yellow.height + yellow.y + laser_velo < 685:
         yellow.y = yellow.y + laser_velo
 
 def handle_bullets(yellow_bullets,red_bullets,yellow,red):
@@ -82,4 +78,43 @@ def handle_bullets(yellow_bullets,red_bullets,yellow,red):
         elif bullet.x < 0:
             yellow_bullets.remove(bullet)
     for bullet in red_bullets:
-        bullet     
+        bullet.x = bullet.x + laser_velo
+        if yellow.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(red_hit))
+            red_bullets.remove(bullet)
+        elif bullet.x > 700:
+            red_bullets.remove(bullet)     
+
+def main():
+    red = pygame.Rect(680,375,battle_ship_width,battle_ship_height)
+    yellow = pygame.Rect(20,375,battle_ship_width,battle_ship_height)
+    red_bullets = []
+    yellow_bullets = []
+    red_ship_health = 20
+    yellow_ship_health = 20
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        clock.tick(fps)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q and len(yellow_bullets) < max_lasershots:
+                    bullet = pygame.Rect(yellow.x + yellow.width ,  yellow.y + yellow.height // 2-2,10,5)
+                    yellow_bullets.append(bullet)
+                elif event.key == pygame.K_m and len(red_bullets) < max_lasershots:
+                    bullet = pygame.Rect(red.x , yellow.y + yellow.height // 2-2,10,5)
+                    red_bullets.append(bullet)
+            elif event.type == yellow_hit:
+                yellow_ship_health = yellow_ship_health - 1
+            elif event.type == red_hit:
+                red_ship_health = red_ship_health - 1
+        winner_text = ""
+        if yellow_ship_health <= 0:
+            winner_text = "The red ship has won this match!"
+        elif red_ship_health <= 0:
+            winner_text = "The yellow ship has won this match"
+        elif winner_text != "":
+            victory(winner_text) # defined Name is victory
--- Ping Pong Computer.py
+++ Ping Pong Computer.py
@@ -9,16 +9,27 @@ pygame.font.init()
 
 pygame.display.set_caption("Ping Pong")
 running = True
+moving_distance = 20
+left_health_score = 20
+right_health_score = 20
 
 while running:
     screen.fill((200,150,255))
+    font = pygame.font.SysFont("Times New Roman", 20)
+    l_healthbar = font.render("Health: " + str(left_health_score), True, (0,0,0))
+    r_healthbar = font.render("Health: " + str(right_health_score), True, (0,0,0))
+    screen.blit(l_healthbar,(15,20))
+    screen.blit(r_healthbar,(400,20))
+    pygame.draw.rect(screen,(0,0,0),(245,0,10,500)) #border
     # pygame.draw.rect(screen,rgb,(x-rect, y-rect, width-rect, height-rect))
-    pygame.draw.rect(screen,(255,255,255),(50,250,10,40)) # left rectangle
-    pygame.draw.rect(screen,(255,255,255),(450,250,10,40)) # right rectangle
+    pygame.draw.rect(screen,(255,255,255),(50,250,20,80)) # left rectangle
+    pygame.draw.rect(screen,(255,255,255),(450,250,20,80)) # right rectangle
     # pygame.draw.circle(screen,rgb,pos,radius,width)
-    pygame.draw.circle(screen,(255,0,0),(250,250),20) # le ball    
+    pygame.draw.circle(screen,(255,0,0),(250,250),20) # le ball   
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         pygame.display.update()
 pygame.quit()
+
+# make padels go up and down by command
